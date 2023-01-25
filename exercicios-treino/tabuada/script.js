c7e07e1bbb0ffda1438a16219c004420cdc133f5
Rafaellauersdorf <117686048+Rafaellauersdorf@users.noreply.github.com>
2023-01-25 10:36:13 -0300

    finalizado ex e iniciado projeto tabuada

---
+++ exercicios-treino/tabuada/script.js
@@ -0,0 +1,15 @@
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if(num.ariaValueMax.length == 0){
+        window.alert('[ATENÇÃO] Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c < 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} x ${n*c}`
+        }
+    }
+}
