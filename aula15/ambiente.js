44b813e50ca29a07d696d17faaed139bad7b671d
Rafaellauersdorf <117686048+Rafaellauersdorf@users.noreply.github.com>
2023-01-27 11:35:07 -0300

    exercicios concluidos

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -4,9 +4,9 @@ num.sort()
 console.log(num)
 console.log(`Nosso vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
-let pos = num.indexOf(1)
+let pos = num.indexOf(9)
 if (pos == -1) {
-    console.log('O valor não foi encontrado!')
+    console.log('O valor não está no vetor!')
 } else {
-    console.log(`O valor está na posição ${pos}`)
+    console.log(`O valor ${num[pos]} está na posição ${pos}`)
 }
